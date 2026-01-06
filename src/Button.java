c9648c67d4715f031f22e95dc0cbbfa89961570a
Alexis <agrischin605@mail.com>
2026-01-06 11:31:22 +0300

    забыл

--- src/Button.java
+++ src/Button.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Button extends JButton {
-
+    // я опять забыл как коммитить и пушить
     Button(String name) {
         super(name);
         this.setFont(new Font("Arial", Font.PLAIN, 20));
